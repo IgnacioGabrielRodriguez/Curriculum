2fc5f67e92f280accc72715d91c8cb69e1359e87
IgnacioGabrielRodriguez <igrodriguez@escuelasproa.edu.ar>
2023-11-28 14:14:52 -0300

    Header + NavBar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
+import { ModeladoComponent } from './pages/modelado/modelado.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {path: 'modelado', component: ModeladoComponent},
   {path:  '', redirectTo: 'home' , pathMatch: 'full'},
 ];
 
